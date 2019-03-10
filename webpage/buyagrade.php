e8eb5a745a201b592d5d137b58fcfe9bf2e1f77d
Makhmudjon <maxmudinadullayev98@gmail.com>
2019-03-10 07:53:25 -0700

    Execise 1 finished

--- webpage/buyagrade.php
+++ webpage/buyagrade.php
@@ -6,6 +6,10 @@
 	</head>
 	
 	<body>
+		<!-- GET METHOD -->
+	<?php
+		if($_SERVER["REQUEST_METHOD"]=="GET") { ?>
+	
 		<h1>Buy Your Way to a Better Education!</h1>
 
 		<p>
@@ -15,25 +19,48 @@
 		<hr />
 		
 		<h2>Give Us Your Money</h2>
+
+		<form method="post">
+			
 		<dl>
 			<dt>Name</dt>
 			<dd>
-				???
+				<input type="text" name="name">
 			</dd>
 			
 			<dt>Section</dt>
 			<dd>
-				???
+				<select>
+					<optgroup label="(Select a selection)">
+						<?php
+							for($x=65; $x<73;$x++){ ?>
+								<option name="<?= "M".chr($x) ?>"><?= "M".chr($x) ?></option>
+						<?php } ?>
+					</optgroup>
+				</select>
+
 			</dd>
 			
 			<dt>Credit Card</dt>
 			<dd>
-				???
+				<input type="text" enabled="false" name="creditCard">
+			</dd>
+			<dd>
+				<input type="radio" name="Visa">Visa
+				<input type="radio" name="MasterCard">MasterCard
 			</dd>
 		</dl>
+
+			<input type="submit" value="I am a giant sucker.">
 		
-		<div>
-			I am a giant sucker.
-		</div>
+	</form>
+
+	<?php
+	} else { 
+			$name = $_REQUEST["name"]; ?>
+
+		<h1>Hello <span><?= $name ?></span> have successfully submited </h1>
+
+		<?php } ?>
 	</body>
 </html>
